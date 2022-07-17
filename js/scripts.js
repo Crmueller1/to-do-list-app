b0254e7f11dbf2d6a6714a90941036811fbc4c40
Crmueller1 <crmueller20@gmail.com>
2022-07-17 16:59:42 -0500

    Crossout-Delete-Sort
    Added 3 functions.
    1.Crossout the text in item by double clicking on the item.
    2. Delete the item on the list by clicking the X button.
    3. Added the ability' to move list items however user wants.

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,21 @@ function newItem() {
         li.toggleClass('strike');
     });
 
+    // Adding delete button x
+
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
+
+    // Connecting CSS Display: NONE to Delete class. 
+
+    crossOutButton.on('click', deletelistItem);
+
+    function deletelistItem() {
+        li.addClass('delete')
+    }
+    // adding the manual ordering of list items
+
+    $('#list').sortable();
+
 }
